6b7d8a8c07a344b3b04924cf82eb80b852ee3d42
nandabalassoni <mariafernandabalassoni@hotmail.com>
2024-06-02 22:56:04 -0300

    creating freelas directory

---
+++ src/pages/Freelas/index.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import './index.css'
+import { useNavigate } from "react-router-dom";
+
+function Freelas() {
+    const [name, setName] = useState();
+    const navigate = useNavigate();
+
+    function handleName() {
+        //setName("Maria");
+        navigate("/freelas"); // Adicione esta linha
+    }
+
+    return (
+        <div className="homeBody">
+            <h1>Find Freelas</h1>
+            {/* <span>{name}</span> */}
+            <p>TESTE PARA PÁGINA DO PROFISSIONAL</p>
+            <button type="button" onClick={handleName} className="findButton">
+                Encontrar Profissionais
+            </button>
+        </div>
+    )
+}
+
+export {Freelas}

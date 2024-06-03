2d612dbf756df8cb31dd7a2568cbb2787b967840
nandabalassoni <mariafernandabalassoni@hotmail.com>
2024-06-02 22:59:47 -0300

    adjusting file for receive a route configuration

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react"
+import './index.css'
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+    const [name, setName] = useState();
+    const navigate = useNavigate();
+
+    function handleName() {
+        //setName("Maria");
+        navigate("/freelas");
+    }
+
     return (
-        <div>
-        <h1>Home</h1>
-        <p>Welcome to the Home page!</p>
+        <div className="homeBody">
+            <h1>Find Freelas</h1>
+            {/* <span>{name}</span> */}
+            <p>O seu portal para encontrar profissionais</p>
+            <button type="button" onClick={handleName} className="findButton">
+                Encontrar Profissionais
+            </button>
         </div>
     )
 }
 
-export { Home}
+export {Home}

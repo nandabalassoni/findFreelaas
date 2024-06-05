8c74056513a389dfab02ff830d51ff32133b96c6
nandabalassoni <mariafernandabalassoni@hotmail.com>
2024-06-05 00:41:00 -0300

    installing bootstrap, configuring and adjusting files

--- src/pages/Freelas/index.tsx
+++ src/pages/Freelas/index.tsx
@@ -1,26 +1,47 @@
 //import { useState } from "react"
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useNavigate } from "react-router-dom";
-import { FindButton, HomeBody } from '../../components/styles'
+import { FindButton, HomeBody } from "../../components/styles";
 
 function Freelas() {
-    //const [name, setName] = useState();
-    const navigate = useNavigate();
+  //const [name, setName] = useState();
+  const navigate = useNavigate();
 
-    function handleName() {
-        //setName("Maria");
-        navigate("/");
-    }
+  function handleName() {
+    //setName("Maria");
+    navigate("/freelas");
+  }
 
-    return (
-        <HomeBody>
-            <h1>Find Freelas</h1>
-            {/* <span>{name}</span> */}
-            <p>TESTE PARA PÁGINA DO PROFISSIONAL</p>
-            <FindButton onClick={handleName} className="findButton">
-                Encontrar Profissionais
-            </FindButton>
-        </HomeBody>
-    )
+  return (
+    <div className="container">
+      <HomeBody>
+        <h1 className="text-center">Find Freelas</h1>
+        {/* <span>{name}</span> */}
+        <p>ENCONTRAR PROFISSIONAIS</p>
+        <FindButton onClick={handleName} className="findButton">
+          Encontrar Profissionais
+        </FindButton>
+
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            className="card-img-top"
+            src=".../100px180/"
+            alt="Imagem de capa do card"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Título do card</h5>
+            <p className="card-text">
+              Um exemplo de texto rápido para construir o título do card e fazer
+              preencher o conteúdo do card.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Visitar
+            </a>
+          </div>
+        </div>
+      </HomeBody>
+    </div>
+  );
 }
 
-export {Freelas}
+export { Freelas };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,26 +1,29 @@
 //import { useState } from "react"
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useNavigate } from "react-router-dom";
-import { FindButton, HomeBody } from '../../components/styles'
+import { FindButton, HomeBody } from "../../components/styles";
 
 function Home() {
-    //const [name, setName] = useState();
-    const navigate = useNavigate();
+  //const [name, setName] = useState();
+  const navigate = useNavigate();
 
-    function handleName() {
-        //setName("Maria");
-        navigate("/freelas");
-    }
+  function handleName() {
+    //setName("Maria");
+    navigate("/freelas");
+  }
 
-    return (
-        <HomeBody>
-            <h1>Find Freelas</h1>
-            {/* <span>{name}</span> */}
-            <p>O seu portal para encontrar profissionais</p>
-            <FindButton onClick={handleName} className="findButton">
-                Encontrar Profissionais
-            </FindButton>
-        </HomeBody>
-    )
+  return (
+    <div className="container">
+      <HomeBody>
+        <h1 className="text-center">Find Freelas</h1>
+        {/* <span>{name}</span> */}
+        <p>O seu portal para encontrar profissionais</p>
+        <FindButton onClick={handleName} className="findButton">
+          Encontrar Profissionais
+        </FindButton>
+      </HomeBody>
+    </div>
+  );
 }
 
-export {Home}
+export { Home };

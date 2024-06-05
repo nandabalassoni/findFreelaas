0c7e74a97def1f72bdfada24363a1b51074c4439
nandabalassoni <mariafernandabalassoni@hotmail.com>
2024-06-05 00:11:16 -0300

    removing unnecessary line

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Freelas } from "./pages/Freelas"; // Importe o componente Freelas
+import { Freelas } from "./pages/Freelas";
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/freelas" element={<Freelas />} /> {/* Defina a rota para o componente Freelas */}
+        <Route path="/freelas" element={<Freelas />} />
       </Routes>
     </Router>
   );
